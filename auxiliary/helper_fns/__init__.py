f8b02255cef46acd3a070d878f2d084c27800384
Vlad <vladislav.brovikov@gmail.com>
2021-11-03 14:09:25 -0400

    added global config file

--- auxiliary/helper_fns/__init__.py
+++ auxiliary/helper_fns/__init__.py
@@ -1,3 +1,10 @@
+
+
+from config import Config
+import sys
+sys.path.append("..")  # Adds higher directory to python modules path.
+
+
 def user_boolean_selection(query, confirmation, rejection):
     response = input(query)
     if response.lower() == "y":
@@ -7,3 +14,21 @@ def user_boolean_selection(query, confirmation, rejection):
         print(rejection)
         return False
     return user_boolean_selection(query, confirmation, rejection)
+
+
+def enable_porter_stemmer(query, confirmation, rejection):
+    response = user_boolean_selection(query, confirmation, rejection)
+    if response:
+        Config.ENABLE_PORTER_STEMMER = True
+    elif not response:
+        Config.ENABLE_PORTER_STEMMER = False
+    print(Config.ENABLE_PORTER_STEMMER)
+
+
+def enable_stop_word_removal(query, confirmation, rejection):
+    response = user_boolean_selection(query, confirmation, rejection)
+    if response:
+        Config.ENABLE_STOP_WORD_REMOVAL = True
+    elif not response:
+        Config.ENABLE_STOP_WORD_REMOVAL = False
+    print(Config.ENABLE_STOP_WORD_REMOVAL)
---
+++ config.py
@@ -0,0 +1,3 @@
+class Config():
+    ENABLE_PORTER_STEMMER = True
+    ENABLE_STOP_WORD_REMOVAL = True
